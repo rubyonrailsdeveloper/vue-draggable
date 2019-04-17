424e8426d2f2440620bb2f1e8102c674ee14656c
Greenfield <webdeveloper1001@outlook.com>
2019-04-16 22:21:42 -0500

    Update vuedraggable.js

--- src/vuedraggable.js
+++ src/vuedraggable.js
@@ -1,10 +1,6 @@
 (function () {
   "use strict";
- if (!Array.from) {
-    Array.from = function (object) {
-      return [].slice.call(object);
-    }
-  }
+
 
   function buildAttribute(object, propName, value) {
     if (value == undefined) {
